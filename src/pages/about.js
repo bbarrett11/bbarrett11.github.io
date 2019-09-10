890d0fa53c05d9b0cee6e327d66187c651097b73
Benjamin Barrett <bjb2us@virginia.edu>
2019-09-10 01:07:18 -0400

    About page change

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const About = () => (
     <h1>Benjamin Jackson Barrett</h1>
     <p>Hello! I'm a third year at the University of Virginia. Double majoring in Computer Science and Math. In addition to
         my majors, I love philosophy, playing music, and reading. Over the past few years I've had some great opportunities
-        and interned at some great companies doing software development work.  
+        and interned at some great companies doing software development work. I primarily have done 
     </p>
   </Layout>
 )
