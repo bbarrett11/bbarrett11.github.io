5d13b7aaad6aefd8e4d054731267c98ed5c4507e
Benjamin Barrett <bjb2us@virginia.edu>
2019-09-11 01:49:40 -0400

    Update about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,17 @@ const About = () => (
     <div style={{padding:"1.3rem"}}>
       <h1>Benjamin Jackson Barrett</h1>
       <p>Hello! I'm a third year at the University of Virginia. Double majoring in Computer Science and Math. In addition to
-        my majors, I love philosophy, playing music, and reading. Over the past few years I've had some great opportunities
-        and interned at some great companies doing software development work. I primarily have done back-end work, but I do have some full stack experience as well.
-         
+        my majors, I love philosophy, playing music, and reading. 
+        These passions have given me an insatiable curiosity, I am always asking "Why?".
+        I'm always learning and trying to improve the answer to this question.
+        I've discovered that my love for CS stems from a desire to build wonderful things.
+        Over the past few years I've had some great opportunities
+        and interned at some great companies doing software development work. 
+        I've primarily done back end work in C#, C++, and Java.
+        The biggest projects I've worked on are pipelines, for both testing and continuous deployment.
+        Working on these complex procedures has allowed me to use a wide breadth of different technologies.  
+        I'd love to work at a place where I can do great work and build wonderful things.     
+      
       </p>
     </div>
   </Layout>
